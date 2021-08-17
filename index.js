8292301930fb7c578501800bba4b18690a09c1c5
Kevin Ebaugh <kevinebaugh@gmail.com>
2021-08-16 23:49:36 -0400

    commit tree 74ca0b33fc46b59f3117ef252986d8f140a6ba56

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+  const data = {
+    "name": name,
+    "email": email
+  }
+  const body = document.querySelector("body")
+  const header = document.createElement("h1")
+
+  console.log("name", name)
+  console.log("email", email)
+  return fetch("http://localhost:3000/users", {
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    "body": JSON.stringify(data)
+  }).then(res => {
+    return res.json()
+  }).then(json => {
+    header.textContent = json.id
+    body.append(header)
+  }).catch(error => {
+    header.textContent = error.message
+    body.append(header)
+  })
+}
